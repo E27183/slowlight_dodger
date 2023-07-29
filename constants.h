88ac266e2510903b4091c1a04177238606d2dfe4
Edan <edan.landow@gmail.com>
2023-07-29 12:22:24 +1000

    full game cycle

--- constants.h
+++ constants.h
@@ -7,3 +7,6 @@ const float boundary_y_high = 10000.0;
 const float boundary_z_high = 10000.0;
 
 const float speed_of_light = 10.0;
+
+const int scenario_max_objects = 256;
+const int millisecond_frame_delay = 100;
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include "constants.h"
 
-void handle_event() {
+void handle_event(gameState *state) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
         switch (event.type) {
@@ -16,10 +16,14 @@ void handle_event() {
     };
 };
 
-struct object {
+struct point {
     float x;
     float y;
     float z;
+};
+
+struct object {
+    point points[16];
     float azimuth;
     float inclination;
     float velocity;
@@ -36,13 +40,28 @@ class gameState {
         float player_x;
         float player_y;
         float player_z;
+        bool accelerating;
         void render(SDL_Renderer* renderer) {
             //Not implemented
+        };
+        void update(float delay) {
+            //Not implemented
+        };
+        void initialise() {
+            this->player_x = 0;
+            this->player_y = 0;
+            this->player_z = 0;
+            this->player_direction_azimuth = 0;
+            this->player_direction_inclination = 0;
+            this->player_velocity = 0;
+            this->object_count = 0;
+            this->accelerating = false;
         };
     float player_direction_azimuth;
     float player_direction_inclination;
     float player_velocity;
-    object objects[256];
+    object objects[scenario_max_objects];
+    int object_count;
 };
 
 int main(int argc, char *argv[]) {  
@@ -56,9 +75,13 @@ int main(int argc, char *argv[]) {
         SDL_WINDOWPOS_CENTERED,
         DisplayMode.w, DisplayMode.h, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED); 
-
+    gameState state;
+    state.initialise();
     while (1) {
-        SDL_Delay(100);
+        handle_event(&state);
+        state.update(millisecond_frame_delay);
+        state.render(renderer);
+        SDL_Delay(millisecond_frame_delay);
     };
 
     return 0;  
